f041b5792910d7820be85126a7488c605fa711e6
Alexander Moeller <alexander.moeller@ein-reisender.de>
2013-07-06 14:04:01 +0200

    fix build
    Signed-off-by: Alexander Moeller <alexander.moeller@ein-reisender.de>

--- KnackDenCode/CodeKnacker.h
+++ KnackDenCode/CodeKnacker.h
@@ -4,16 +4,13 @@
 
 #include "ui_CodeKnacker.h"
 
-class CodeKnacker : public QMainWindow, public UI_CodeKnacker
+class CodeKnacker : public QMainWindow, public Ui::UI_CodeKnacker
 {
 	Q_OBJECT
 	
 	public:
 		CodeKnacker(QMainWindow *parent = 0);
 		~CodeKnacker();
-		
-		//TODO
 
 	private:
-		//TODO
 };
