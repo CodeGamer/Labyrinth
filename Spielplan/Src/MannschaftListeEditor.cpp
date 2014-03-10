6a45a35c82249fe25137aeea6e3e99a010e3a908
Pascal Schimkus <mister-p20@hotmail.de>
2014-03-10 19:02:09 +0100

    MannschaftListeEditor mit Beispielen
    Signed-off-by: Pascal Schimkus <mister-p20@hotmail.de>

--- Spielplan/Src/MannschaftListeEditor.cpp
+++ Spielplan/Src/MannschaftListeEditor.cpp
@@ -1,3 +1,5 @@
+#include <QObject>
+
 #include "MannschaftListeEditor.h"
 
 MannschaftListeEditor::MannschaftListeEditor(QDialog *parent)
@@ -5,6 +7,17 @@ MannschaftListeEditor::MannschaftListeEditor(QDialog *parent)
 {
 	setupUi(this);
 
+	mannschaftListeTable->setRowCount(2);
+	mannschaftListeTable->setColumnCount(2);
+
+	mannschaftListeTable->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+	mannschaftListeTable->setHorizontalHeaderLabels(QString("Mannschaft;Herkunft").split(";"));
+
+	mannschaftListeTable->setItem(0, 0, new QTableWidgetItem("Hannover 1"));
+	mannschaftListeTable->setItem(0, 1, new QTableWidgetItem("Hannover"));
+	mannschaftListeTable->setItem(1, 0, new QTableWidgetItem("Hannover 2"));
+	mannschaftListeTable->setItem(1, 1, new QTableWidgetItem("Hannover"));
 }
 
 MannschaftListeEditor::~MannschaftListeEditor()
